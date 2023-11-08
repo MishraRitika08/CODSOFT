5f6c3be4555cbce679194ba2e48194da9f03f3c0
Ritika Mishra <mishra.r.0804@gmail.com>
2023-11-08 11:56:25 +0530

    added play again feature

--- numberGuess.java
+++ numberGuess.java
@@ -1,29 +1,39 @@
 import java.util.*;
-class numberGuess{
-    public static void main(String args[]){
+
+class NumberGuess {
+    public static void game(int k) {
         Scanner sc = new Scanner(System.in);
-        int k = 10;
-        int n = (int) (Math.random() * 100) + 1;
-      
-        while(k>0){
-              System.out.println("Enter a number\n");
-            //System.out.println("Guess again");
-            int g = sc.nextInt();
-            if(g==n){
-                System.out.println("HOORAY! you guessed the number right\n");
+        while (true) {
+            int n = (int) (Math.random() * 100) + 1;
+            while (k > 0) {
+                System.out.println("Enter a number: ");
+                int g = sc.nextInt();
+                if (g == n) {
+                    System.out.println("HOORAY! You guessed the number right.");
+                    break;
+                } else if (g > n) {
+                    System.out.println("The number you entered is greater than the number I guessed.");
+                } else {
+                    System.out.println("The number you entered is less than the number I guessed. Give it another try.");
+                }
+                k--;
+            }
+            if (k == 0) {
+                System.out.println("The number was " + n);
+            }
+            System.out.println("Want to play again? Press '+' or any other key to exit.");
+            char s = sc.next().charAt(0);
+            if (s != '+') {
                 break;
             }
-            else if (g>n) {
-                System.out.println("The number you entered is greater than the number I guessed\n");
-            }
-            else{
-                System.out.println("The number you entered is less than the number I guessed \n give it another try\n");
-            }
-            k--;
         }
-        if(k==0){
-            System.out.println("The number was"+ n);
-        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of attempts you want: ");
+        int k = sc.nextInt();
+        game(k);
         sc.close();
     }
 }
