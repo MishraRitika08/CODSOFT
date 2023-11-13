1f9924f64ee2b25e837ca782a8d8407f2851d422
Ritika Mishra <mishra.r.0804@gmail.com>
2023-11-13 16:55:26 +0530

    compleated task2

--- gradeCalculator.java
+++ gradeCalculator.java
@@ -1,18 +1,20 @@
+import java.util.Scanner;
+
 public class gradeCalculator {
     public static int calculatePercentage(int totalMarks) {
-        int percentage = totalMarks/400;
+        int percentage = totalMarks/4;
         return percentage;
     }
-    public static char calculateGrade() {
-        char grade;
-        int percentage = calculatePercentage();
-        if(percentage>90){
+    public static char calculateGrade(int totalMarks) {
+        char grade=' ';
+        int percentage = calculatePercentage(totalMarks);
+        if(percentage>=90){
             grade ='A';
         }
-        if(percentage<90 && percentage>80){
+        else if(percentage<90 && percentage>=80){
             grade ='B';
         }
-        if(percentage<80 && percentage>70){
+        else if(percentage<80 && percentage>=70){
             grade ='C';
         }
         else{
@@ -20,14 +22,17 @@ public class gradeCalculator {
         }
         return grade;
     }
-
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int sub1=sc.nextInt();
         int sub2=sc.nextInt();
         int sub3=sc.nextInt();
         int sub4=sc.nextInt();
         int totalMarks = sub1 + sub2 + sub3 + sub4 ;
+        char grade=calculateGrade(totalMarks);
+        int percentage=calculatePercentage(totalMarks);
+        System.out.print("The total marks is "+ totalMarks +
+                         " \nThe percentage is "+ percentage+"%"+ "\nThe grade is "+grade);
         
     }
 }
